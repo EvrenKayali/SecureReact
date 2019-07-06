f9b961eb8f6e8c509fdf603a7558fcabe3d192f7
Evren Kayali <evren.kayali@readify.net>
2019-07-07 00:04:23 +1000

    clean styles

--- src/Client/src/components/header.tsx
+++ src/Client/src/components/header.tsx
@@ -1,15 +1,16 @@
 import React from "react";
+import styles from "./header.module.css";
 
 export const Header: React.FC = () => (
-  <header className="header">
+  <header className={styles.header}>
     <h3>Secure React App</h3>
     <nav>
       {/* <ul>
-          <li>item 1</li>
-          <li>item 2</li>
-          <li>item 3</li>
-          <li>item 4</li>
-        </ul> */}
+        <li>item 1</li>
+        <li>item 2</li>
+        <li>item 3</li>
+        <li>item 4</li>
+      </ul> */}
       <div>Evren Kayali</div>
     </nav>
   </header>
